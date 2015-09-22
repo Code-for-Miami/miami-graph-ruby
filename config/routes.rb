486b94b30b236398586cdb3b8078ee0d81997605
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-21 20:41:59 -0400

    Add pages with working links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'abouts#home'
   get '/about' => 'abouts#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
